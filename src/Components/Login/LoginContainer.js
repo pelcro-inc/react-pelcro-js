59d7a32cd76caf7ee2325897602049542bab1906
Amr Sekilly <amr@pelcro.com>
2020-01-20 18:25:35 +0200

    Added the email validation

--- src/Components/Login/LoginContainer.js
+++ src/Components/Login/LoginContainer.js
@@ -1,16 +1,32 @@
 import React, { createContext, useReducer } from "react";
+import {
+  SET_EMAIL,
+  SET_PASSWORD,
+  SET_EMAIL_ERROR,
+  SET_PASSWORD_ERROR
+} from "../../utils/action-types";
 
-const initialState = { email: "", password: "" };
+const initialState = {
+  email: "",
+  password: "",
+  emailError: null,
+  passwordError: null
+};
 const store = createContext(initialState);
 const { Provider } = store;
 
 const LoginContainer = ({ style, className, children }) => {
   const [state, dispatch] = useReducer((state, action) => {
     switch (action.type) {
-      case "setEmail":
-        return { ...state, email: action.payload };
-      case "setPassword":
-        return { ...state, password: action.payload };
+      case SET_EMAIL:
+        return { ...state, email: action.payload, emailError: null };
+      case SET_PASSWORD:
+        return { ...state, password: action.payload, passwordError: null };
+      case SET_EMAIL_ERROR:
+        console.log("error: ", action.payload);
+        return { ...state, emailError: action.payload };
+      case SET_PASSWORD_ERROR:
+        return { ...state, passwordError: action.payload };
       default:
         throw new Error();
     }
--- src/SubComponents/Email.js
+++ src/SubComponents/Email.js
@@ -1,21 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
+import { SET_EMAIL, SET_EMAIL_ERROR } from "../utils/action-types";
 
 export default function Email({ placeholder, style, className, id, store }) {
-  const { dispatch, state } = useContext(store);
+  const { dispatch } = useContext(store);
+  const [email, setEmail] = useState("");
+  const [finishedTyping, setFinishedTyping] = useState(false);
 
-  const handleInputChange = value => {
-    dispatch({ type: "setEmail", payload: value });
+  const handleInputChange = useCallback(
+    value => {
+      setEmail(value);
+
+      if (validateEmail(email)) {
+        dispatch({ type: SET_EMAIL, payload: value });
+      } else if (finishedTyping) {
+        dispatch({
+          type: SET_EMAIL_ERROR,
+          payload: "Please enter a valid email."
+        });
+      }
+    },
+    [dispatch, email, finishedTyping]
+  );
+
+  useEffect(() => {
+    handleInputChange(email);
+  }, [finishedTyping, email, handleInputChange]);
+
+  const validateEmail = email => {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
   };
 
   return (
     <input
-      type="text"
+      type="email"
       id={id}
       style={{ ...style }}
       className={className}
-      value={state.email}
+      value={email}
       onChange={e => handleInputChange(e.target.value)}
       placeholder={placeholder || "Enter Your Email"}
+      onBlur={() => setFinishedTyping(true)}
+      onFocus={() => setFinishedTyping(false)}
     ></input>
   );
 }
