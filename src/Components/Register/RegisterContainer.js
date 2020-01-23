485f8084e9b81481f0105f732d79a5f7faeec408
Amr Sekilly <amr@pelcro.com>
2020-01-23 15:27:33 +0200

    Added the confirm password component

--- src/Components/Register/RegisterContainer.js
+++ src/Components/Register/RegisterContainer.js
@@ -4,14 +4,20 @@ import {
   SET_PASSWORD,
   SET_EMAIL_ERROR,
   SET_PASSWORD_ERROR,
-  RESET_LOGIN_FORM
+  RESET_LOGIN_FORM,
+  SET_CONFIRM_PASSWORD,
+  CONFIRM_PASSWORD_USED,
+  SET_CONFIRM_PASSWORD_ERROR
 } from "../../utils/action-types";
 
 const initialState = {
   email: "",
   password: "",
   emailError: null,
-  passwordError: null
+  passwordError: null,
+  confirmPassword: "",
+  confirmPasswordError: null,
+  confirmPasswordUsed: false
 };
 const store = createContext(initialState);
 const { Provider } = store;
@@ -23,12 +29,27 @@ const RegisterContainer = ({ style, className, children }) => {
         return { ...state, email: action.payload, emailError: null };
       case SET_PASSWORD:
         return { ...state, password: action.payload, passwordError: null };
+      case SET_CONFIRM_PASSWORD:
+        return {
+          ...state,
+          confirmPassword: action.payload,
+          confirmPasswordError: null
+        };
       case SET_EMAIL_ERROR:
         return { ...state, emailError: action.payload, email: "" };
       case SET_PASSWORD_ERROR:
         return { ...state, passwordError: action.payload, password: "" };
+      case SET_CONFIRM_PASSWORD_ERROR:
+        return {
+          ...state,
+          confirmPasswordError: action.payload,
+          confirmPassword: ""
+        };
+      case CONFIRM_PASSWORD_USED:
+        return { ...state, confirmPasswordUsed: action.payload };
       case RESET_LOGIN_FORM:
         return initialState;
+
       default:
         throw new Error();
     }
---
+++ src/SubComponents/ConfirmPassword.js
@@ -0,0 +1,56 @@
+import React, { useContext, useState, useEffect, useCallback } from "react";
+import {
+  SET_CONFIRM_PASSWORD,
+  SET_CONFIRM_PASSWORD_ERROR,
+  CONFIRM_PASSWORD_USED
+} from "../utils/action-types";
+
+export function ConfirmPassword({ placeholder, style, className, id, store }) {
+  const {
+    dispatch,
+    state: { confirmPassword: stateConfirmPassword, confirmPasswordError }
+  } = useContext(store);
+  const [password, setPassword] = useState(stateConfirmPassword);
+  const [finishedTyping, setFinishedTyping] = useState(false);
+
+  useEffect(() => dispatch({ type: CONFIRM_PASSWORD_USED, payload: true }), [
+    dispatch
+  ]);
+
+  const handleInputChange = useCallback(
+    value => {
+      setPassword(value);
+
+      if (password.length) {
+        dispatch({ type: SET_CONFIRM_PASSWORD, payload: password });
+      } else if (finishedTyping) {
+        dispatch({
+          type: SET_CONFIRM_PASSWORD_ERROR,
+          payload: "Confirm password is required."
+        });
+      }
+    },
+    [dispatch, password, finishedTyping]
+  );
+
+  useEffect(() => {
+    handleInputChange(password);
+  }, [finishedTyping, password, handleInputChange]);
+
+  return (
+    <React.Fragment>
+      <input
+        type="password"
+        id={id}
+        style={{ ...style }}
+        className={confirmPasswordError ? "input-error " : "" + className}
+        value={password}
+        onChange={e => handleInputChange(e.target.value)}
+        placeholder={placeholder || "Enter Your Password again"}
+        onBlur={() => setFinishedTyping(true)}
+        onFocus={() => setFinishedTyping(false)}
+      ></input>
+      <div>{confirmPasswordError}</div>
+    </React.Fragment>
+  );
+}
