e1fb078805d126ce671c73240a5727b096d72139
Amr Sekilly <amr@pelcro.com>
2020-02-18 15:02:24 +0200

    Refactoring the styles

--- src/Components/Payment/PaymentModal.js
+++ src/Components/Payment/PaymentModal.js
@@ -1,35 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PaymentView } from "./PaymentView";
 import Header from "../common/Header";
 import Authorship from "../common/Authorship";
+import styles from "./styles.module.scss";
 
-export const PaymentModal = props => (
-  <div className="pelcro-prefix-view">
-    <div
-      className="pelcro-prefix-modal modal pelcro-prefix-fade pelcro-prefix-show"
-      id="pelcro-view-payment"
-      tabIndex="-1"
-      role="dialog"
-      aria-hidden="true"
-    >
+export const PaymentModal = props => {
+  const [site, setSite] = useState(0);
+
+  useEffect(() => {
+    setSite(window.Pelcro.site.read());
+  }, []);
+
+  return (
+    <div className="pelcro-prefix-view">
       <div
-        className="pelcro-prefix-modal-dialog pelcro-prefix-modal-dialog-centered"
-        role="document"
+        className="pelcro-prefix-modal modal pelcro-prefix-fade pelcro-prefix-show"
+        id="pelcro-view-payment"
+        tabIndex="-1"
+        role="dialog"
+        aria-hidden="true"
       >
-        <div className="pelcro-prefix-modal-content">
-          <Header
-            closeButton={true}
-            resetView={this.props.resetView}
-            site={window.Pelcro.site.read()}
-          ></Header>
-          <div className="pelcro-prefix-modal-body">
-            <PaymentView {...props} />
-          </div>
-          <div className="pelcro-prefix-modal-footer">
-            <Authorship></Authorship>
+        <div
+          className="pelcro-prefix-modal-dialog pelcro-prefix-modal-dialog-centered"
+          role="document"
+        >
+          <div className="pelcro-prefix-modal-content">
+            <Header
+              closeButton={true}
+              resetView={props.resetView}
+              site={site}
+            ></Header>
+            <div className="pelcro-prefix-modal-body">
+              <PaymentView {...props} />
+            </div>
+            <div className="pelcro-prefix-modal-footer">
+              <Authorship></Authorship>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
