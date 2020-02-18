e2b813d29e41b6b257cfd2ab6c2ed61829504f1e
Amr Sekilly <amr@pelcro.com>
2020-02-18 18:49:58 +0200

    Fixed the target library

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.min.js",
-    libraryTarget: "commonjs2"
+    libraryTarget: "umd"
   },
   module: {
     rules: [
