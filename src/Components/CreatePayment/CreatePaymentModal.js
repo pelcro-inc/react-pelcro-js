75a3ae4ae93cfe9876335ec9f778c470f9994a23
Amr Sekilly <amr@pelcro.com>
2020-03-13 14:31:16 +0200

    Changed the type of the payment modal after it was refactored

--- src/Components/CreatePayment/CreatePaymentModal.js
+++ src/Components/CreatePayment/CreatePaymentModal.js
@@ -5,7 +5,7 @@ import Authorship from "../common/Authorship";
 
 import { CreatePaymentView } from "./CreatePaymentView";
 
-export class CreatePaymentModal extends Component {
+export function CreatePaymentModal() {
   // showCouponField = () => {
   //   this.setState({
   //     enableCouponField: !this.state.enableCouponField
@@ -16,56 +16,54 @@ export class CreatePaymentModal extends Component {
   //   this.setState({ couponCode: event.target.value });
   // }
 
-  render() {
-    return (
-      <div className="pelcro-prefix-view">
+  return (
+    <div className="pelcro-prefix-view">
+      <div
+        className="pelcro-prefix-modal modal pelcro-prefix-fade pelcro-prefix-show"
+        id="pelcro-view-payment"
+        tabIndex="-1"
+        role="dialog"
+        aria-hidden="true"
+      >
         <div
-          className="pelcro-prefix-modal modal pelcro-prefix-fade pelcro-prefix-show"
-          id="pelcro-view-payment"
-          tabIndex="-1"
-          role="dialog"
-          aria-hidden="true"
+          className="pelcro-prefix-modal-dialog pelcro-prefix-modal-dialog-centered"
+          role="document"
         >
-          <div
-            className="pelcro-prefix-modal-dialog pelcro-prefix-modal-dialog-centered"
-            role="document"
-          >
-            <div className="pelcro-prefix-modal-content">
-              <Header
-                closeButton={window.Pelcro.paywall.displayCloseButton()}
-                resetView={this.props.resetView}
-                site={window.Pelcro.site.read()}
-              ></Header>
+          <div className="pelcro-prefix-modal-content">
+            <Header
+              closeButton={window.Pelcro.paywall.displayCloseButton()}
+              resetView={this.props.resetView}
+              site={window.Pelcro.site.read()}
+            ></Header>
 
-              <div className="pelcro-prefix-modal-body">
-                <CreatePaymentView />
-              </div>
-              <div className="pelcro-prefix-modal-footer">
-                <small>
-                  {this.locale.messages.haveQuestion}{" "}
-                  {this.locale.messages.visitOurFaq.visitOur}{" "}
-                  <a
-                    className="pelcro-prefix-link"
-                    target="new"
-                    href="https://www.pelcro.com/faq/user"
-                  >
-                    {this.locale.messages.visitOurFaq.faq}
-                  </a>
-                  . {this.locale.messages.cancel}
-                  {" " + this.locale.messages.logout.logout}{" "}
-                  <button
-                    className="pelcro-prefix-link"
-                    onClick={this.props.logout}
-                  >
-                    {this.locale.messages.logout.here}
-                  </button>
-                </small>
-                <Authorship></Authorship>
-              </div>
+            <div className="pelcro-prefix-modal-body">
+              <CreatePaymentView />
+            </div>
+            <div className="pelcro-prefix-modal-footer">
+              <small>
+                {this.locale.messages.haveQuestion}{" "}
+                {this.locale.messages.visitOurFaq.visitOur}{" "}
+                <a
+                  className="pelcro-prefix-link"
+                  target="new"
+                  href="https://www.pelcro.com/faq/user"
+                >
+                  {this.locale.messages.visitOurFaq.faq}
+                </a>
+                . {this.locale.messages.cancel}
+                {" " + this.locale.messages.logout.logout}{" "}
+                <button
+                  className="pelcro-prefix-link"
+                  onClick={this.props.logout}
+                >
+                  {this.locale.messages.logout.here}
+                </button>
+              </small>
+              <Authorship></Authorship>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
