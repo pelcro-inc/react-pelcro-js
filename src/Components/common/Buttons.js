69b773854361c5aab502a9487c0f2fcd2dd00525
Amr Sekilly <amr@pelcro.com>
2020-03-14 17:31:40 +0200

    Added the new buttons without the deprecated ones

--- src/Components/common/Buttons.js
+++ src/Components/common/Buttons.js
@@ -1,40 +1,9 @@
-import localisation from "../../utils/localisation";
+import localisation from "../../services/localisation";
 
 const locale = localisation("buttons").getLocaleData();
 
 export const init = app => {
   // if there are Login and Subscribe buttons on the page, we add event listeners which open modal windows
-  const loginButton = document.getElementById("login-button");
-
-  if (loginButton) {
-    loginButton.addEventListener("click", app.displayLoginView);
-  }
-
-  const registerButton = document.getElementById("register-button");
-
-  if (registerButton) {
-    registerButton.addEventListener("click", app.displayRegisterView);
-  }
-
-  const cartButton = document.getElementById("cart-button");
-
-  if (cartButton) {
-    cartButton.addEventListener("click", app.displayCartView);
-  }
-
-  const subscribeButton = document.getElementById("subscribe-button");
-
-  if (subscribeButton) {
-    subscribeButton.addEventListener("click", app.displaySelectView);
-  }
-
-  const loginButtonsByClass = document.getElementsByClassName("login-button");
-
-  if (loginButtonsByClass.length !== 0) {
-    for (let i = 0; i < loginButtonsByClass.length; i++) {
-      loginButtonsByClass[i].addEventListener("click", app.displayLoginView);
-    }
-  }
 
   const pelcroLoginButtonsByClass = document.getElementsByClassName(
     "pelcro-login-button"
@@ -49,33 +18,10 @@ export const init = app => {
     }
   }
 
-  const subscribeButtonsByClass = document.getElementsByClassName(
-    "subscribe-button"
-  );
+  const cartButton = document.getElementById("pelcro-cart-button");
 
-  if (subscribeButtonsByClass.length !== 0) {
-    for (let j = 0; j < subscribeButtonsByClass.length; j++) {
-      if (
-        subscribeButtonsByClass[j].dataset &&
-        "productId" in subscribeButtonsByClass[j].dataset &&
-        "planId" in subscribeButtonsByClass[j].dataset
-      ) {
-        subscribeButtonsByClass[j].addEventListener(
-          "click",
-          app.setProductAndPlanByButton
-        );
-      } else if (
-        subscribeButtonsByClass[j].dataset &&
-        "productId" in subscribeButtonsByClass[j].dataset
-      ) {
-        subscribeButtonsByClass[j].addEventListener("click", app.setProduct);
-      } else {
-        subscribeButtonsByClass[j].addEventListener(
-          "click",
-          app.displaySelectView
-        );
-      }
-    }
+  if (cartButton) {
+    cartButton.addEventListener("click", app.displayCartView);
   }
 
   const pelcroSubscribeButtonsByClass = document.getElementsByClassName(
@@ -110,19 +56,6 @@ export const init = app => {
     }
   }
 
-  const registerButtonsByClass = document.getElementsByClassName(
-    "register-button"
-  );
-
-  if (registerButtonsByClass.length !== 0) {
-    for (let j = 0; j < registerButtonsByClass.length; j++) {
-      registerButtonsByClass[j].addEventListener(
-        "click",
-        app.displayRegisterView
-      );
-    }
-  }
-
   const pelcroRegisterButtonsByClass = document.getElementsByClassName(
     "pelcro-register-button"
   );
@@ -132,29 +65,6 @@ export const init = app => {
       pelcroRegisterButtonsByClass[j].addEventListener(
         "click",
         app.displayRegisterView
-      );
-    }
-  }
-
-  const productButtonsByClass = document.getElementsByClassName(
-    "pelcro-product-button"
-  );
-
-  if (productButtonsByClass.length !== 0) {
-    for (let j = 0; j < productButtonsByClass.length; j++) {
-      productButtonsByClass[j].addEventListener("click", app.setProduct);
-    }
-  }
-
-  const planButtonsByClass = document.getElementsByClassName(
-    "pelcro-plan-button"
-  );
-
-  if (planButtonsByClass.length !== 0) {
-    for (let j = 0; j < planButtonsByClass.length; j++) {
-      planButtonsByClass[j].addEventListener(
-        "click",
-        app.setProductAndPlanByButton
       );
     }
   }
@@ -171,25 +81,11 @@ export const init = app => {
 };
 
 export const authenticatedButtons = id => {
-  const loginById = document.getElementById("login-button");
-
-  if (loginById) {
-    loginById.innerHTML = locale.account;
-  }
-
-  const loginByClass = document.getElementsByClassName("login-button");
-
-  if (loginByClass) {
-    for (let i = 0; i < loginByClass.length; i++) {
-      loginByClass.item(i).innerHTML = locale.account;
-    }
-  }
-
   const pelcroLoginByClass = document.getElementsByClassName(
     "pelcro-login-button"
   );
 
-  if (loginByClass) {
+  if (pelcroLoginByClass) {
     for (let i = 0; i < pelcroLoginByClass.length; i++) {
       pelcroLoginByClass.item(i).innerHTML = locale.account;
     }
@@ -197,20 +93,6 @@ export const authenticatedButtons = id => {
 };
 
 export const unauthenticatedButtons = id => {
-  const loginById = document.getElementById("login-button");
-
-  if (loginById) {
-    loginById.innerHTML = locale.login;
-  }
-
-  const loginByClass = document.getElementsByClassName("login-button");
-
-  if (loginByClass) {
-    for (let i = 0; i < loginByClass.length; i++) {
-      loginByClass.item(i).innerHTML = locale.login;
-    }
-  }
-
   const pelcroLoginByClass = document.getElementsByClassName(
     "pelcro-login-button"
   );
