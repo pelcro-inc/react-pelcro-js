ac09c9cfd6a181e8c05c91537dc2c6e34ce6252a
Amr Sekilly <amr@pelcro.com>
2020-06-27 06:51:29 +0200

    Fix CouponCode.js to be more modularized & hide/show bug

--- src/Components/PaymentMethod/CouponCode.js
+++ src/Components/PaymentMethod/CouponCode.js
@@ -1,25 +1,23 @@
 import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { ApplyCouponButton } from "./ApplyCouponButton";
+import { CouponCodeField } from "./CouponCodeField";
 import { store } from "./PaymentMethodContainer";
-import {
-  SHOW_COUPON_FIELD,
-  UPDATE_COUPON_CODE,
-} from "../../utils/action-types";
+import { SHOW_COUPON_FIELD } from "../../utils/action-types";
 
 export const CouponCode = ({ showCoupon }) => {
   const {
     dispatch,
-    state: { enableCouponField, couponCode, disableCouponButton },
+    state: { enableCouponField, disableCouponButton }
   } = useContext(store);
 
   const { t } = useTranslation("checkoutForm");
 
   const showCouponField = () =>
-    dispatch({ type: SHOW_COUPON_FIELD, payload: true });
-
-  const onCouponCodeChange = (e) =>
-    dispatch({ type: UPDATE_COUPON_CODE, payload: e.target.value });
+    dispatch({
+      type: SHOW_COUPON_FIELD,
+      payload: !enableCouponField
+    });
 
   if (showCoupon) {
     return (
@@ -30,7 +28,9 @@ export const CouponCode = ({ showCoupon }) => {
           onClick={showCouponField}
           style={!enableCouponField ? { marginBottom: 10 } : {}}
         >
-          {!enableCouponField ? t("labels.addCode") : t("labels.hideCode")}
+          {!enableCouponField
+            ? t("labels.addCode")
+            : t("labels.hideCode")}
         </button>
         {enableCouponField && (
           <div className="pelcro-prefix-row">
@@ -42,19 +42,14 @@ export const CouponCode = ({ showCoupon }) => {
                 >
                   {t("labels.code")}
                 </label>
-                <input
-                  value={couponCode}
-                  onChange={onCouponCodeChange}
-                  className="pelcro-prefix-input pelcro-prefix-form-control"
-                  type="text"
-                  placeholder={t("labels.codePlaceholder")}
-                  id="pelcro-input-coupon_code"
-                />
+                <CouponCodeField />
               </div>
             </div>
             <div className="col-sm-12 apply-coupon-button">
               <div className="pelcro-prefix-input-wrapper">
-                <ApplyCouponButton disableCouponButton={disableCouponButton}>
+                <ApplyCouponButton
+                  disableCouponButton={disableCouponButton}
+                >
                   {t("labels.applyCouponCode")}
                 </ApplyCouponButton>
               </div>
---
+++ src/Components/PaymentMethod/CouponCodeField.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { useTranslation } from "react-i18next";
+import { store } from "./PaymentMethodContainer";
+import { UPDATE_COUPON_CODE } from "../../utils/action-types";
+
+export const CouponCodeField = props => {
+  const { t } = useTranslation("checkoutForm");
+  const {
+    dispatch,
+    state: { couponCode }
+  } = useContext(store);
+
+  const onCouponCodeChange = e =>
+    dispatch({ type: UPDATE_COUPON_CODE, payload: e.target.value });
+
+  return (
+    <input
+      value={couponCode}
+      onChange={onCouponCodeChange}
+      className="pelcro-prefix-input pelcro-prefix-form-control"
+      type="text"
+      placeholder={t("labels.codePlaceholder")}
+      id="pelcro-input-coupon_code"
+      {...props}
+    />
+  );
+};
