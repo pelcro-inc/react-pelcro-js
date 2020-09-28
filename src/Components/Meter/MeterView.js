3ce1b60b53651cdec02ccf1fea9c61cb6a37b7c4
Khaled Albarawy <khaled.albarawi@gmail.com>
2020-09-28 12:38:04 +0200

    [FIX] Add access safety for meter props + add br between fields

--- src/Components/Meter/MeterView.js
+++ src/Components/Meter/MeterView.js
@@ -3,14 +3,15 @@ import { useTranslation } from "react-i18next";
 
 export const MeterView = (props) => {
   const { t } = useTranslation("meter");
-
-  const plan =
-    props.plan ||
-    (window.Pelcro.paywall.getProduct() &&
-      window.Pelcro.paywall.getProduct().plans[0]);
-  const product = props.product || window.Pelcro.paywall.getProduct();
-  const visitsLeft = window.Pelcro.paywall.freeVisitsLeft();
   const site = window.Pelcro.site.read();
+  const plan =
+    props.plan ?? window.Pelcro.paywall.getProduct()?.plans?.[0];
+  const product = props.product ?? window.Pelcro.paywall.getProduct();
+  const visitsLeft = window.Pelcro.paywall.freeVisitsLeft();
+
+  const title =
+    plan && `${product?.paywall?.meter_title}: ${visitsLeft}`;
+  const subtitle = plan && product?.paywall?.meter_subtitle;
 
   const displayLoginView = () => {
     props.setView("login");
@@ -30,22 +31,28 @@ export const MeterView = (props) => {
         ></img>
       )}
       <div>
-        <h4>
-          {plan && product.paywall.meter_title}: {visitsLeft}
-        </h4>
+        <h4>{title}</h4>
         <p>
-          {plan && product.paywall.meter_subtitle}{" "}
-          <button className="pelcro-prefix-link" onClick={displaySelectView}>
+          {subtitle}{" "}
+          <button
+            className="pelcro-prefix-link"
+            onClick={displaySelectView}
+          >
             {t("messages.subscribeNow")}
           </button>
           {!window.Pelcro.user.isAuthenticated() && (
-            <span>
-              {" " + t("messages.alreadyHaveAccount") + " "}
-              <button className="pelcro-prefix-link" onClick={displayLoginView}>
-                {" "}
-                {t("messages.loginHere")}
-              </button>
-            </span>
+            <>
+              <br />
+              <span>
+                {t("messages.alreadyHaveAccount") + " "}
+                <button
+                  className="pelcro-prefix-link"
+                  onClick={displayLoginView}
+                >
+                  {t("messages.loginHere")}
+                </button>
+              </span>
+            </>
           )}
         </p>
       </div>
