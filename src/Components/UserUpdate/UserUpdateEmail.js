57699643d45def7888a2e0bc2809a019248af738
Khaled Albarawy <khaled.albarawi@gmail.com>
2020-11-02 15:29:57 +0200

    [FIX] Initialize email field on PelcroUserLoaded event and on display

--- src/Components/UserUpdate/UserUpdateEmail.js
+++ src/Components/UserUpdate/UserUpdateEmail.js
@@ -2,11 +2,10 @@ import React from "react";
 import { store } from "./UserUpdateContainer";
 import { Email } from "../../SubComponents/Email";
 
-export const UserUpdateEmail = props => (
+export const UserUpdateEmail = (props) => (
   <Email
     disabled={true}
     id="pelcro-login-email"
-    value={window.Pelcro.user.read().email}
     store={store}
     {...props}
   />
