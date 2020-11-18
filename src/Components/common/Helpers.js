cc7bae413c2b9b964ffe357eab2bb87823e56278
Khaled Albarawy <khaled.albarawi@gmail.com>
2020-11-18 20:55:13 +0200

    [FEAT] Add inhouse debounce function to not use lodash

--- src/Components/common/Helpers.js
+++ src/Components/common/Helpers.js
@@ -24,10 +24,30 @@ export const getErrorMessages = (error) => {
   return errorMessages.join("\n");
 };
 
+/**
+ * Executes function after a wait time of inactivity
+ * @param {function} func
+ * @param {number} waitTime
+ */
+export const debounce = (func, waitTime) => {
+  let timeout;
 
-  values(error?.response?.data?.errors).forEach((message) => {
-    messages.push(message[0]);
-  });
+  return function executedFunction(...args) {
+    // The callback function to be executed after
+    // the debounce time has elapsed
+    const later = () => {
+      // null timeout to indicate the debounce ended
+      timeout = null;
+      func(...args);
+    };
+    // This will reset the waiting every function execution.
+    // This is the step that prevents the function from
+    // being executed because it will never reach the
+    // inside of the previous setTimeout
+    clearTimeout(timeout);
 
-  return messages.join("\n");
+    // Restart the debounce waiting period.
+    // setTimeout returns a truthy value (it differs in web vs Node)
+    timeout = setTimeout(later, waitTime);
+  };
 };
