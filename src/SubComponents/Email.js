532e2c3427b497acf75aaf69e57d013980425fac
Khaled Albarawy <khaled.albarawi@gmail.com>
2021-01-14 13:57:45 +0200

    [REFACTOR] Clean up branches @Email component

--- src/SubComponents/Email.js
+++ src/SubComponents/Email.js
@@ -11,6 +11,7 @@ import { SET_EMAIL, SET_EMAIL_ERROR } from "../utils/action-types";
  */
 export function Email({
   initWithUserEmail = true,
+  disableEmailValidation,
   placeholder,
   style,
   className,
@@ -29,23 +30,25 @@ export function Email({
     (value) => {
       setEmail(value);
 
-      if (otherProps.disableEmailValidation) {
-        dispatch({ type: SET_EMAIL, payload: email });
-      } else {
-        if (validateEmail(email)) {
-          dispatch({ type: SET_EMAIL, payload: email });
-        } else if (finishedTyping) {
-          if (email?.length) {
-            dispatch({
-              type: SET_EMAIL_ERROR,
-              payload: "Please enter a valid email."
-            });
-          } else {
-            dispatch({
-              type: SET_EMAIL_ERROR,
-              payload: "Email address is required."
-            });
-          }
+      if (disableEmailValidation) {
+        return dispatch({ type: SET_EMAIL, payload: email });
+      }
+
+      if (isEmailValid(email)) {
+        return dispatch({ type: SET_EMAIL, payload: email });
+      }
+
+      if (finishedTyping) {
+        if (email?.length) {
+          dispatch({
+            type: SET_EMAIL_ERROR,
+            payload: "Please enter a valid email."
+          });
+        } else {
+          dispatch({
+            type: SET_EMAIL_ERROR,
+            payload: "Email address is required."
+          });
         }
       }
     },
@@ -77,7 +80,7 @@ export function Email({
     }
   }, []);
 
-  const validateEmail = (email) => {
+  const isEmailValid = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   };
@@ -90,7 +93,7 @@ export function Email({
         style={{ ...style }}
         className={(emailError ? "input-error " : "") + className}
         value={
-          (otherProps?.disableEmailValidation
+          (disableEmailValidation
             ? stateEmail
             : email || stateEmail) ?? ""
         }
