3914fb37b0a769c5c5059c2ea86ed2a5e125eab2
Khaled Albarawy <ProgaPanda@users.noreply.github.com>
2021-01-22 21:49:28 +0200

    [ENHANCEMENT] Migrate order confirm to use i18n service  (#45)
    * [FEAT] Add shop trans files
    
    * [REFACTOR] Use i18n instead of local translation service
    
    * [FEAT] Add shop fr translation wording

--- src/components.js
+++ src/components.js
@@ -122,7 +122,7 @@ export { OrderCraeteSubmitButton } from "./Components/OrderCreate/OrderCraeteSub
 export { OrderCreateContainer } from "./Components/OrderCreate/OrderCreateContainer";
 export { OrderCreateModal } from "./Components/OrderCreate/OrderCreateModal";
 export { OrderCreateView } from "./Components/OrderCreate/OrderCreateView";
-export { OrderConfirmModal } from "./Components/OrderConfirm/OrderConfirmModal";
+export { OrderConfirmModalWithTrans as OrderConfirmModal } from "./Components/OrderConfirm/OrderConfirmModal";
 
 export { GiftCreateModal } from "./Components/GiftCreate/GiftCreateModal";
 export { GiftCreateView } from "./Components/GiftCreate/GiftCreateView";
