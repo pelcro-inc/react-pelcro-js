bc66673c4882d7a4ca2da1d315812951999ee450
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-02-05 20:34:32 +0200

    [FEAT] add Tailwind base, and [REFACTOR] remove sass files

--- src/components.js
+++ src/components.js
@@ -1,4 +1,6 @@
 import "./i18n";
+import "./index.css";
+
 export { LoginContainer } from "./Components/Login/LoginContainer";
 export { Email } from "./SubComponents/Email";
 export { Password } from "./SubComponents/Password";
