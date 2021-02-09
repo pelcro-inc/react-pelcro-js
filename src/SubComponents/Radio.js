fd6a2e2cc8075bfc83c27e25f7a192a6c2643b16
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-02-09 06:10:54 +0200

    [FEAT] initial implementation of radio atomic component

---
+++ src/SubComponents/Radio.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Radio({
+  className = "",
+  id,
+  children,
+  ...otherProps
+}) {
+  return (
+    <div className="flex items-center">
+      <input
+        type="radio"
+        id={id}
+        className={`text-green-500 checked:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 text-md disabled:cursor-not-allowed disabled:text-gray-300 ${className}`}
+        {...otherProps}
+      />
+      <label htmlFor={id} className="ml-2">
+        {children}
+      </label>
+    </div>
+  );
+}
