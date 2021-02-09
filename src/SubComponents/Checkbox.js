018b1008f3633ca619e5d3f7cfde573dea6c1bd7
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-02-09 06:11:10 +0200

    [FEAT] initial implementation of checkbox atomic component

---
+++ src/SubComponents/Checkbox.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Checkbox({
+  className = "",
+  id,
+  children,
+  ...otherProps
+}) {
+  return (
+    <div className="flex items-center">
+      <input
+        type="checkbox"
+        id={id}
+        className={`text-green-500 checked:border-transparent rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 text-md disabled:cursor-not-allowed disabled:text-gray-300 pelcro-checkbox-input ${className}`}
+        {...otherProps}
+      />
+      <label htmlFor={id} className="ml-2 pelcro-checkbox-label">
+        {children}
+      </label>
+    </div>
+  );
+}
