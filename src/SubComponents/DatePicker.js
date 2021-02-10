2a7b5cc45ef853892423d39dc06e17d597e9d0e4
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-02-10 04:25:45 +0200

    [FEAT] initial implementation of date picker atomic component

---
+++ src/SubComponents/DatePicker.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export function DatePicker({
+  label = "",
+  required,
+  id,
+  errorId,
+  error,
+  className = "",
+  labelClassName = "",
+  errorClassName = "",
+  ...otherProps
+}) {
+  return (
+    <div className="w-full">
+      <label
+        htmlFor={id}
+        className={`text-gray-700 pelcro-prefix-label pelcro-date-label ${labelClassName}`}
+      >
+        {`${label}${required ? "*" : ""}`}
+      </label>
+      <input
+        type="date"
+        id={id}
+        className={`mt-1 w-full border border-gray-300 bg-gray-50 p-3 text-sm appearance-none outline-none rounded-sm focus:ring-1 focus:ring-blue-600 disabled:bg-gray-300 disabled:cursor-not-allowed pelcro-prefix-input pelcro-prefix-form-control pelcro-date-input ${className} ${
+          error ? "ring-1 ring-red-600 input-error" : ""
+        }`}
+        aria-describedby={errorId}
+        aria-invalid={Boolean(error)}
+        {...otherProps}
+      />
+      <p
+        id={errorId}
+        className={`h-4 my-2 text-red-600 normal-case pelcro-field-error ${errorClassName}`}
+      >
+        {error}
+      </p>
+    </div>
+  );
+}
