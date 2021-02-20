e113f39ac8ec492b4d37afb677055946f1f749ea
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-02-20 19:06:56 +0200

    [FEAT] move address update modal to new CSS structure

--- src/Components/AddressUpdate/AddressUpdateModal.js
+++ src/Components/AddressUpdate/AddressUpdateModal.js
@@ -1,37 +1,30 @@
 import React from "react";
-import Header from "../common/Header";
+import {
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader
+} from "../../SubComponents/Modal";
 import Authorship from "../common/Authorship";
 import { AddressUpdateView } from "./AddressUpdateView";
 
-export const AddressUpdateModal = (props) => {
+export const AddressUpdateModal = (onClose, ...otherProps) => {
+  const site = window.Pelcro.site.read();
+
   return (
-    <div className="pelcro-prefix-view">
-      <div
-        className="pelcro-prefix-modal pelcro-prefix-fade pelcro-prefix-show"
-        id="pelcro-view-address"
-        tabIndex="-1"
-        role="dialog"
-        aria-hidden="true"
-      >
-        <div
-          className="pelcro-prefix-modal-dialog pelcro-prefix-modal-dialog-centered"
-          role="document"
-        >
-          <div className="pelcro-prefix-modal-content">
-            <Header
-              closeButton={window.Pelcro.paywall.displayCloseButton()}
-              resetView={props.resetView}
-              site={window.Pelcro.site.read()}
-            ></Header>
-            <div className="pelcro-prefix-modal-body">
-              <AddressUpdateView {...props} />
-            </div>
-            <div className="pelcro-prefix-modal-footer">
-              <Authorship></Authorship>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Modal id="pelcro-address-update-modal">
+      <ModalHeader
+        hideCloseButton={!window.Pelcro.paywall.displayCloseButton()}
+        onClose={onClose}
+        logo={site.logo}
+        title={site.name}
+      />
+      <ModalBody>
+        <AddressUpdateView {...otherProps} />
+      </ModalBody>
+      <ModalFooter>
+        <Authorship />
+      </ModalFooter>
+    </Modal>
   );
 };
