115dc03a78a6994bd7b0483438ecbf95b009772d
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-04-19 15:11:43 +0200

    [FEAT] convert authorship logo to a link

--- src/Components/common/Authorship.js
+++ src/Components/common/Authorship.js
@@ -2,7 +2,15 @@ import React from "react";
 import { ReactComponent as AuthorshipIcon } from "../../assets/pelcro-authorship.svg";
 
 const Authorship = () => {
-  return <AuthorshipIcon className="plc-h-12 pelcro-authorship" />;
+  return (
+    <a
+      href={`https://www.pelcro.com/?utm_source=${window.location.hostname}&utm_medium=direct&utm_campaign=powered_by_pelcro`}
+      rel="noopener noreferrer nofollow"
+      target="_blank"
+    >
+      <AuthorshipIcon className="plc-h-12 pelcro-authorship" />
+    </a>
+  );
 };
 
 export default Authorship;
