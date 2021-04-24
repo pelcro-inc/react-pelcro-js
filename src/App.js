c777960846706c6133e4efe605be979d099b4e21
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-04-24 06:06:41 +0200

    [FEAT] load site's custom theming on mount

--- src/App.js
+++ src/App.js
@@ -119,9 +119,79 @@ class App extends Component {
   };
 
   initUI = () => {
+    this.initTheming();
     if (this.state.site.settings === "subscription") {
       this.initViews();
     }
+  };
+
+  initTheming = () => {
+    // copied from https://css-tricks.com/converting-color-spaces-in-javascript/#hex-to-hsl
+    const hexToHsl = (H) => {
+      // Convert hex to RGB first
+      let r = 0,
+        g = 0,
+        b = 0;
+      if (H.length == 4) {
+        r = "0x" + H[1] + H[1];
+        g = "0x" + H[2] + H[2];
+        b = "0x" + H[3] + H[3];
+      } else if (H.length == 7) {
+        r = "0x" + H[1] + H[2];
+        g = "0x" + H[3] + H[4];
+        b = "0x" + H[5] + H[6];
+      }
+      // Then to HSL
+      r /= 255;
+      g /= 255;
+      b /= 255;
+      let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+
+      if (delta == 0) h = 0;
+      else if (cmax == r) h = ((g - b) / delta) % 6;
+      else if (cmax == g) h = (b - r) / delta + 2;
+      else h = (r - g) / delta + 4;
+
+      h = Math.round(h * 60);
+
+      if (h < 0) h += 360;
+
+      l = (cmax + cmin) / 2;
+      s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+      s = +(s * 100).toFixed(1);
+      l = +(l * 100).toFixed(1);
+
+      return {
+        hue: String(h),
+        saturation: `${s}%`,
+        lightness: `${l}%`
+      };
+    };
+
+    const primaryColorHex = this.state.site.design_settings
+      .primary_color;
+    if (!primaryColorHex) {
+      return;
+    }
+
+    const primaryColorHsl = hexToHsl(primaryColorHex);
+    document.documentElement.style.setProperty(
+      "--plc-primary-hue",
+      primaryColorHsl.hue
+    );
+    document.documentElement.style.setProperty(
+      "--plc-primary-saturation",
+      primaryColorHsl.saturation
+    );
+    document.documentElement.style.setProperty(
+      "--plc-primary-lightness",
+      primaryColorHsl.lightness
+    );
   };
 
   initViewFromUrl = () => {
