b02237dc21ea753a7d231f83e362b9b0833e2819
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-04-30 07:33:35 +0200

    [REFACTOR] use apply directive for modal subcomp

--- src/SubComponents/Modal.js
+++ src/SubComponents/Modal.js
@@ -10,20 +10,20 @@ export function Modal({
   ...otherProps
 }) {
   return (
-    <div className="plc-fixed plc-top-0 plc-left-0 plc-flex plc-items-center plc-justify-center plc-w-full plc-h-full plc-overflow-auto plc-bg-black pelcro-modal-overlay plc-z-max plc-bg-opacity-60">
+    <div className="pelcro-modal-overlay">
       <div
-        className={`plc-container plc-absolute plc-top-0 plc-flex plc-flex-col plc-items-center plc-max-w-lg plc-bg-white plc-border-t-8 plc-border-primary-500 plc-rounded-md md:plc-mt-8 pelcro-modal ${className}`}
+        className={`pelcro-modal ${className}`}
         role="dialog"
         aria-modal="true"
         id={id}
         {...otherProps}
       >
-        <div className="plc-w-full pelcro-modal-content">
-          <div className="plc-p-1 pelcro-modal-header">
+        <div className="pelcro-modal-content">
+          <div className="pelcro-modal-header">
             {!hideCloseButton && (
               <button
                 type="button"
-                className="plc-absolute plc-w-6 plc-text-gray-500 plc-z-max plc-top-3 plc-right-3 hover:plc-text-black pelcro-modal-close"
+                className="pelcro-modal-close"
                 aria-label="close modal"
                 onClick={onClose}
               >
@@ -40,17 +40,9 @@ export function Modal({
 }
 
 export const ModalBody = ({ children }) => {
-  return (
-    <div className="plc-mx-8 plc-my-4 pelcro-modal-body">
-      {children}
-    </div>
-  );
+  return <div className="pelcro-modal-body">{children}</div>;
 };
 
 export const ModalFooter = ({ children }) => {
-  return (
-    <div className="plc-flex plc-flex-col plc-items-center plc-justify-center plc-mx-8 plc-mt-6 plc-mb-4 plc-text-sm pelcro-modal-footer">
-      {children}
-    </div>
-  );
+  return <div className="pelcro-modal-footer">{children}</div>;
 };
