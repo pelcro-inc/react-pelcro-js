3137009d55317ef89af71e0174b632665dac1b01
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-05-21 23:10:11 +0200

    [FEAT] replace outline icon with a solid one, and change title color to gray

--- src/Components/PaymentSuccess/PaymentSuccessView.js
+++ src/Components/PaymentSuccess/PaymentSuccessView.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Button } from "../../SubComponents/Button";
-import { ReactComponent as CheckMarkOutlineIcon } from "../../assets/check-outline.svg";
+import { ReactComponent as CheckMark } from "../../assets/check-solid.svg";
 
 export const PaymentSuccessView = ({ onClose, product }) => {
   const { t } = useTranslation("success");
@@ -9,9 +9,9 @@ export const PaymentSuccessView = ({ onClose, product }) => {
   if (product) {
     return (
       <div className="plc-flex plc-flex-col plc-items-center">
-        <CheckMarkOutlineIcon className="plc-w-32 plc-my-4 plc-text-green-500" />
+        <CheckMark className="plc-w-32 plc-my-4 plc-text-green-500" />
         <div className="plc-text-center plc-text-gray-900">
-          <h4 className="plc-mb-4 plc-text-3xl plc-text-green-500">
+          <h4 className="plc-mb-4 plc-text-3xl">
             {product.paywall.success_title}
           </h4>
           <p>{product.paywall.success_content}</p>
