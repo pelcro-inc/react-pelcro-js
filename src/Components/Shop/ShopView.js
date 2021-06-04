0118eac07244cfe42060fb9d6cf254854d67c98c
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-06-04 21:23:57 +0200

    [REFACTOR] change shopView to be a simple view without a container

--- src/Components/Shop/ShopView.js
+++ src/Components/Shop/ShopView.js
@@ -1,46 +1,43 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
-import { ShopContainer } from "./ShopContainer";
+import { getAllSkus } from "../../utils/utils";
 import { ShopSelectProductButton } from "./ShopSelectProductButton";
 
-export const ShopView = (props) => {
-  const { t } = useTranslation("shop");
+export const ShopView = ({ addCartItem }) => {
+  const skus = getAllSkus();
 
   return (
     <div id="pelcro-shop-view">
-      <ShopContainer {...props}>
-        <div className="plc-grid plc-justify-center plc-rounded plc-justify-items-center plc-gap-y-5 plc-gap-x-3 pelcro-shop-products">
-          {props.products.map((product) => {
-            return (
-              <div
-                key={product.id}
-                className="plc-flex plc-flex-col plc-items-center plc-p-2 plc-m-3 plc-border plc-border-gray-400 plc-border-solid plc-rounded-md plc-w-max pelcro-shop-product-wrapper"
-              >
-                {product.image && (
-                  <img
-                    className="pelcro-shop-product-image"
-                    alt={`image of ${product.name}`}
-                    src={product.image}
-                  />
-                )}
-                <div className="plc-flex plc-flex-col plc-items-center plc-mt-auto pelcro-shop-product-info-wrapper">
-                  <p className="plc-font-bold pelcro-shop-product-name">
-                    {product.name}
-                  </p>
-                  <p className="plc-font-bold pelcro-shop-product-description">
-                    {product.description}
-                  </p>
-                  <ShopSelectProductButton
-                    product={product}
-                    name={t("buttons.select")}
-                    className="plc-mt-2"
-                  />
-                </div>
+      <div className="plc-grid plc-justify-center plc-rounded plc-justify-items-center plc-gap-y-5 plc-gap-x-3 pelcro-shop-products">
+        {skus.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="plc-flex plc-flex-col plc-items-center plc-p-2 plc-m-3 plc-border plc-border-gray-400 plc-border-solid plc-rounded-md plc-w-max pelcro-shop-product-wrapper"
+            >
+              {item.image && (
+                <img
+                  className="pelcro-shop-product-image"
+                  alt={`image of ${item.name}`}
+                  src={item.image}
+                />
+              )}
+              <div className="plc-flex plc-flex-col plc-items-center plc-mt-auto pelcro-shop-product-info-wrapper">
+                <p className="plc-font-bold pelcro-shop-product-name">
+                  {item.name}
+                </p>
+                <p className="plc-font-bold pelcro-shop-product-description">
+                  {item.description}
+                </p>
+                <ShopSelectProductButton
+                  itemId={item.id}
+                  addCartItem={addCartItem}
+                  className="plc-mt-2"
+                />
               </div>
-            );
-          })}
-        </div>
-      </ShopContainer>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
