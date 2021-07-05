225892ac3d75551c7a55266998bef4a1fc8dc085
Khaled Albarawy <khaled.albarawi@gmail.com>
2021-07-05 13:32:37 +0200

    feat(social): add social login buttons

---
+++ src/Components/common/GoogleLoginButton/GoogleLoginButton.js
@@ -0,0 +1,63 @@
+import React, { useContext } from "react";
+import { GoogleLogin } from "react-google-login";
+import { store as loginStore } from "../../Login/LoginContainer";
+import { store as registerStore } from "../../Register/RegisterContainer";
+import { ReactComponent as GoogleLogoIcon } from "../../../assets/google-logo.svg";
+import { HANDLE_SOCIAL_LOGIN } from "../../../utils/action-types";
+
+export const GoogleLoginButton = () => {
+  const googleLoginEnabled = window.Pelcro.site.read()?.google_app_id;
+
+  const { dispatch: loginDispatch } = useContext(loginStore);
+  const { dispatch: registerDispatch } = useContext(registerStore);
+
+  const onSuccess = (response) => {
+    const profile = response.getBasicProfile();
+    const accessToken = response.getAuthResponse()?.access_token;
+
+    loginDispatch?.({
+      type: HANDLE_SOCIAL_LOGIN,
+      payload: {
+        idpName: "google",
+        idpToken: accessToken,
+        email: profile.getEmail?.(),
+        firstName: profile.getGivenName?.(),
+        lastName: profile.getFamilyName?.()
+      }
+    });
+
+    registerDispatch?.({
+      type: HANDLE_SOCIAL_LOGIN,
+      payload: {
+        idpName: "google",
+        idpToken: accessToken,
+        email: profile.getEmail?.(),
+        firstName: profile.getGivenName?.(),
+        lastName: profile.getFamilyName?.()
+      }
+    });
+  };
+
+  const onFailure = (error) => {
+    console.error(error);
+  };
+
+  return (
+    googleLoginEnabled && (
+      <GoogleLogin
+        clientId={window.Pelcro.site.read().google_app_id}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        render={(renderProps) => (
+          <button
+            onClick={renderProps.onClick}
+            className="plc-flex plc-items-center plc-justify-center plc-w-full plc-p-3 plc-space-x-3 plc-text-gray-700 plc-border plc-border-gray-200 plc-rounded-3xl hover:plc-bg-gray-200"
+          >
+            <GoogleLogoIcon className="plc-w-6 plc-h-auto" />
+            <p>Google</p>
+          </button>
+        )}
+      />
+    )
+  );
+};
