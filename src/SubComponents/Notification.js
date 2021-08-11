e3b87b2f418e57ad461c6ea1d9ecc111c1908caa
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-08-11 04:14:30 +0200

    feat(notification comp): add warning notification + close button

--- src/SubComponents/Notification.js
+++ src/SubComponents/Notification.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { default as toast, Toaster } from "react-hot-toast";
+import { default as toast, ToastBar, Toaster } from "react-hot-toast";
 import { ReactComponent as CheckIcon } from "../assets/check-solid.svg";
-import { ReactComponent as XIcon } from "../assets/x-icon-solid.svg";
+import { ReactComponent as SolidXIcon } from "../assets/x-icon-solid.svg";
+import { ReactComponent as SolidExclamationIcon } from "../assets/exclamation.svg";
+import { ReactComponent as XIcon } from "../assets/x-icon.svg";
 
 export const Notification = ({ children, ...otherProps }) => {
   return (
@@ -18,7 +20,7 @@ export const Notification = ({ children, ...otherProps }) => {
         },
         error: {
           className: "pelcro-notification-error",
-          icon: <XIcon className="plc-w-20 plc-h-8" />,
+          icon: <SolidXIcon className="plc-w-20 plc-h-8" />,
           iconTheme: {
             primary: "white",
             secondary: "red"
@@ -27,11 +29,35 @@ export const Notification = ({ children, ...otherProps }) => {
       }}
       {...otherProps}
     >
-      {children}
+      {(t) => (
+        <ToastBar toast={t}>
+          {({ icon, message }) => (
+            <>
+              {icon}
+              {message}
+              <XIcon
+                className="plc-w-10 plc-h-5 plc-cursor-pointer"
+                onClick={() => toast.dismiss(t.id)}
+              />
+            </>
+          )}
+        </ToastBar>
+      )}
     </Toaster>
   );
 };
 
 Notification.viewId = "notification";
+
+toast.warning = (msg, options) =>
+  toast(msg, {
+    className: "pelcro-notification-warning",
+    icon: <SolidExclamationIcon className="plc-w-24 plc-h-8" />,
+    iconTheme: {
+      primary: "white",
+      secondary: "yellow"
+    },
+    ...options
+  });
 
 export const notify = toast;
