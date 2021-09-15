4560c499dc30d4293e3392e91b420e11c2d9fd2d
Khaled Albarawy <khaled.albarawi@gmail.com>
2021-09-15 17:26:48 +0200

    fix(tooltip): use an svg icon instead of the symbol

--- src/SubComponents/Tooltip.js
+++ src/SubComponents/Tooltip.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { ReactComponent as InfoCircleIcon } from "../assets/info-circle.svg";
 
 /**
  *
  */
 export const Tooltip = ({ value, className }) => {
   return (
-    <div className={`plc-flex pelcro-tooltip-container ${className}`}>
-      <div className="plc-text-xl pelcro-tooltip-icon">🛈</div>
-      <div className="plc-p-1 plc--mt-8 plc-text-sm plc-text-gray-600 plc-bg-gray-100 plc-border plc-rounded-md plc-shadow-lg pelcro-tooltip">
+    <div
+      className={`plc-flex pelcro-tooltip-container plc-text-gray-600 ${className}`}
+    >
+      <div className="pelcro-tooltip-icon">
+        <InfoCircleIcon />
+      </div>
+      <div className="plc-p-1 plc--mt-8 plc-text-sm plc-bg-gray-100 plc-border plc-rounded-md plc-shadow-lg pelcro-tooltip">
         {value}
       </div>
     </div>
