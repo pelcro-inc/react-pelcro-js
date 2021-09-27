a509b0b9c4a2dd60f5412633f575ca3c1de72773
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2021-09-27 15:54:09 +0200

    feat(entitlements): handle hard-coded combo + improve error handling

--- src/Components/common/contentEntitlement/index.js
+++ src/Components/common/contentEntitlement/index.js
@@ -3,44 +3,66 @@ import { usePelcro } from "../../../hooks/usePelcro";
 import { notify } from "../../../SubComponents/Notification";
 import { Trans } from "react-i18next";
 import { Link } from "../../../SubComponents/Link";
+import { getProductsWithEntitlements } from "../../../utils/utils";
 
 export const init = () => {
-  const { whenSiteReady, view, resetView, switchView } =
-    usePelcro.getStore();
+  const {
+    whenSiteReady,
+    whenUserReady, //FIXME: use other one
+    view,
+    resetView,
+    switchView,
+    set
+  } = usePelcro.getStore();
 
-  whenSiteReady(() => {
+  whenUserReady(() => {
     const entitlementsProtectedElements = document.querySelectorAll(
-      "[data-entitlement]"
+      "[data-entitlements]"
     );
 
-    entitlementsProtectedElements.forEach((elem) => {
-      const shouldBlurContent = !window.Pelcro.user.isEntitledTo(
-        elem.dataset.entitlement
-      );
+    if (entitlementsProtectedElements.length === 0) {
+      return;
+    }
 
-      if (shouldBlurContent) {
+    if (
+      !allElemsHaveSameEntitlements(entitlementsProtectedElements)
+    ) {
+      console.error(
+        "not all elements protected by entitlements have the same entitlements, all elements protected by entitlements must have the exact same data-entitlements attribute value"
+      );
+      return;
+    }
+
+    entitlementsProtectedElements.forEach((elem) => {
+      const entitlements = getEntitlementsFromElem(elem);
+
+      if (entitlements.length === 0) {
+        console.error(
+          "invalid data-entitlements attribute value",
+          elem
+        );
+        return;
+      }
+
+      if (getProductsWithEntitlements(entitlements).length === 0) {
+        console.warn(
+          "user can't subscribe to any plan that has any of the entitlement(s) needed, this is usually unintentional, make sure that the entitlements are spelled correctly, entitlements are case sensitive. make sure that your plans are configured in a way that allows users from all supported countries, using all supported currencies, to have access to your content by subscribing to certain plans that provide the needed entitlement(s)"
+        );
+      }
+
+      if (shouldBlurContent(entitlements)) {
         // remove all event listeners from the elem by replacing it with a deep clone of itself
         const elemDeepClone = elem.cloneNode(true);
         elem.replaceWith(elemDeepClone);
 
-        // disable keyboard interaction with blured content
-        elemDeepClone.addEventListener(
-          "keydown",
-          (event) => {
-            // allow tab key navigation to prevent focus trapping
-            const TAB_KEY_CODE = 9;
-            if (event.keyCode != TAB_KEY_CODE) {
-              event.returnValue = false;
-              return false;
-            }
-          },
-          true
-        );
+        disableKeyboardInteractions(elemDeepClone);
 
         elemDeepClone.setAttribute(
           "style",
           "filter:blur(3px) !important; pointer-events:none !important; user-select:none !important"
         );
+
+        unblurElemWhenUserSubscribes(elemDeepClone, entitlements);
 
         /* 
         showing both the meter and the entitlements notification doesn't make sense from
@@ -59,6 +81,26 @@ export const init = () => {
               or more of our plans.
               <Link
                 onClick={() => {
+                  const productsList = window.Pelcro.product.list();
+                  const productId = elem.dataset.productId;
+                  const planId = elem.dataset.planId;
+
+                  const selectedProduct = productsList.find(
+                    (product) => product.id === Number(productId)
+                  );
+                  const selectedPlan = selectedProduct?.plans?.find(
+                    (plan) => plan.id === Number(planId)
+                  );
+                  const hasValidProductAndPlan = Boolean(
+                    selectedProduct && selectedPlan
+                  );
+
+                  if (hasValidProductAndPlan) {
+                    set({
+                      product: selectedProduct,
+                      plan: selectedPlan
+                    });
+                  }
                   notify.dismiss(NOTIFICATION_ID);
                   switchView("plan-select");
                 }}
@@ -79,3 +121,61 @@ export const init = () => {
     });
   });
 };
+
+function allElemsHaveSameEntitlements(elems) {
+  const entitlements = getEntitlementsFromElem(elems[0]);
+
+  return Array.from(elems).every((elem) => {
+    const elemEntitlements = getEntitlementsFromElem(elem);
+    if (elemEntitlements.length !== entitlements.length) return false;
+    return entitlements.every((ent) =>
+      elemEntitlements.includes(ent)
+    );
+  });
+}
+
+function getEntitlementsFromElem(elem) {
+  return elem.dataset.entitlements
+    .split(",")
+    .map((entitlement) => entitlement.trim())
+    .filter((entitlement) => entitlement);
+}
+
+function disableKeyboardInteractions(elem) {
+  elem.addEventListener(
+    "keydown",
+    (event) => {
+      // allow tab key navigation to prevent focus trapping
+      const TAB_KEY_CODE = 9;
+      if (event.keyCode != TAB_KEY_CODE) {
+        event.returnValue = false;
+        return false;
+      }
+    },
+    true
+  );
+}
+
+function unblurElemWhenUserSubscribes(elem, entitlements) {
+  document.addEventListener("PelcroSubscriptionCreate", (event) => {
+    const { isGift } = usePelcro.getStore();
+    if (isGift) return;
+    const latestSub = event.detail.data.subscriptions[0];
+    const shouldUnblurContent = entitlements.some(
+      (ent) => latestSub.plan.entitlements?.includes(ent) ?? false
+    );
+
+    if (shouldUnblurContent) {
+      elem.setAttribute(
+        "style",
+        "filter:inherit !important; pointer-events:inherit !important; user-select:inherit !important"
+      );
+    }
+  });
+}
+
+function shouldBlurContent(entitlements) {
+  return entitlements.every(
+    (entitlement) => !window.Pelcro.user.isEntitledTo(entitlement)
+  );
+}
