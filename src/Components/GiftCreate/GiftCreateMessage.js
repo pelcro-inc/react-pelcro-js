2cc52fcf2cc55e9b8cfc256f8c98c5391c937ad5
Khaled Albarawy <ProgaPanda@users.noreply.github.com>
2021-10-05 18:06:57 +0200

    feat(textarea): add tooltip text support (#92)

--- src/Components/GiftCreate/GiftCreateMessage.js
+++ src/Components/GiftCreate/GiftCreateMessage.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useTranslation } from "react-i18next";
 import { store } from "./GiftCreateContainer";
 import { Loader } from "../../SubComponents/Loader";
 import { SET_GIFT_MESSAGE } from "../../utils/action-types";
@@ -8,6 +9,7 @@ import { TextArea } from "../../SubComponents/TextArea";
  *
  */
 export function GiftCreateMessage(props) {
+  const { t } = useTranslation("register");
   const { dispatch, state } = useContext(store);
 
   const handleInputChange = (value) => {
@@ -24,6 +26,7 @@ export function GiftCreateMessage(props) {
       value={state.giftMessage}
       maxLength="500"
       rows={3}
+      tooltipText={t("gift.messages.giftMessageInfo")}
       {...props}
     />
   );
