2af86a25623c5c2ef9d1a51d60c692e4ceb53b83
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2022-01-12 03:51:17 +0200

    fix(edit profile modal): phone field can't be cleared (#123)
    Co-authored-by: Ahmed Al-Obaidi <a.mudhar@pelcro.com>

--- src/SubComponents/Phone.js
+++ src/SubComponents/Phone.js
@@ -1,23 +1,21 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { SET_PHONE } from "../utils/action-types";
 import { Input } from "./Input";
 
 export function Phone({ store, ...otherProps }) {
   const {
     dispatch,
-    state: { phone: stateValue }
+    state: { phone }
   } = useContext(store);
-  const [value, setValue] = useState(stateValue);
 
   const handleInputChange = (value) => {
-    setValue(value);
     dispatch({ type: SET_PHONE, payload: value });
   };
 
   return (
     <Input
       type="tel"
-      value={value || window.Pelcro.user.read().phone}
+      value={phone}
       onChange={(e) => handleInputChange(e.target.value)}
       {...otherProps}
     />
