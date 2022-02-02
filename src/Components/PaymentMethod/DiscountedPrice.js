d53dacf493200784d236c0c773d098c1d1c957c0
Khaled Albarawy <khaled.albarawi@gmail.com>
2022-02-02 14:05:12 +0200

    feat(discount): add a remove coupon button

--- src/Components/PaymentMethod/DiscountedPrice.js
+++ src/Components/PaymentMethod/DiscountedPrice.js
@@ -1,13 +1,18 @@
 import React, { useContext } from "react";
+import { useTranslation } from "react-i18next";
 import { usePelcro } from "../../hooks/usePelcro";
 import {
   getFormattedPriceByLocal,
   getPageOrDefaultLanguage
 } from "../../utils/utils";
 import { store } from "./PaymentMethodContainer";
+import { REMOVE_APPLIED_COUPON } from "../../utils/action-types";
+import { Button } from "../../SubComponents/Button";
 
 export const DiscountedPrice = (props) => {
+  const { t } = useTranslation("checkoutForm");
   const {
+    dispatch,
     state: { updatedPrice, taxAmount, percentOff }
   } = useContext(store);
   const { order, plan } = usePelcro();
@@ -22,16 +27,27 @@ export const DiscountedPrice = (props) => {
     getPageOrDefaultLanguage()
   );
 
+  const onCouponRemove = () => {
+    dispatch({ type: REMOVE_APPLIED_COUPON });
+  };
+
   if (percentOff) {
     return (
       <div
-        className="plc-flex plc-justify-center plc-mt-2 pelcro-discount"
+        className="plc-flex plc-items-center plc-justify-center plc-mt-2 pelcro-discount"
         {...props}
       >
-        (-{percentOff}){" "}
-        <span className="plc-font-bold pelcro-discounted-price">
+        (-{percentOff})
+        <span className="plc-ml-1 plc-font-bold pelcro-discounted-price">
           {priceFormatted}
         </span>
+        <Button
+          variant="ghost"
+          className="plc-ml-2 plc-text-red-500 focus:plc-ring-red-500 pelcro-dashboard-sub-cancel-button"
+          onClick={onCouponRemove}
+        >
+          ✕ {t("labels.removeCoupon")}
+        </Button>
       </div>
     );
   }
