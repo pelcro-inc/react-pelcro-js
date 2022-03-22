ebf2e34d408a51da2d519b3f9269868f3f2ae168
Ahmed Al-Obaidi <a.mudhar@pelcro.com>
2022-03-22 04:54:13 +0200

    fix(main.js): remove left-over subsOption modal

--- src/Main.js
+++ src/Main.js
@@ -59,7 +59,6 @@ export const Main = () => {
       <AddressSelectModal />
       <AddressUpdateModal />
 
-      <SubscriptionOptionsModal />
       <SubscriptionCreateModal />
       <SubscriptionRenewModal />
       <PaymentSuccessModal />
