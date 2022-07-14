15aaeb9e9c0d803352fa413118af352b22046d2d
Mohamed Khaled <104787713+pelcro-mkhaled@users.noreply.github.com>
2022-07-14 14:00:28 +0200

    fix(SelectModal): add (s) to print format (#172)

--- src/Components/Select/SelectModal.js
+++ src/Components/Select/SelectModal.js
@@ -144,9 +144,9 @@ class SelectModal extends Component {
     }
     return `${startingPlan.amount_formatted}/${
       startingPlan.interval_count > 1
-        ? `${startingPlan.interval_count} `
-        : ""
-    }${startingPlan.interval}`;
+        ? `${startingPlan.interval_count} ${startingPlan.interval}s`
+        : `${startingPlan.interval}`
+    }`;
   };
 
   renderOneProduct = (product, index, options) => {
