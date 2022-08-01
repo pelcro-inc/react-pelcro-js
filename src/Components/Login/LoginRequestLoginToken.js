972430bd4375357d4758e7623c8de7c5c0c9b143
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2022-08-01 19:23:41 +0200

    feat: redesign the login modal

--- src/Components/Login/LoginRequestLoginToken.js
+++ src/Components/Login/LoginRequestLoginToken.js
@@ -2,26 +2,27 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { ReactComponent as PasswordlessLogoIcon } from "../../assets/email-verify.svg";
 
-export const LoginRequestLoginToken = ({ 
-  name, 
-  onClick, 
-  className = "", 
-  labelClassName = "", 
-  iconClassName = "",  
-  ...otherProps 
+export const LoginRequestLoginToken = ({
+  name,
+  onClick,
+  className = "",
+  labelClassName = "",
+  iconClassName = "",
+  ...otherProps
 }) => {
-
   const { t } = useTranslation("passwordlessRequest");
 
   return (
     <button
       onClick={() => onClick?.()}
-      className={`plc-flex plc-items-center plc-mt-3 plc-mx-auto plc-justify-center plc-p-3 plc-space-x-3 plc-text-gray-700 plc-border plc-border-gray-200 plc-rounded-3xl hover:plc-bg-gray-200 pelcro-auth0-login ${className}`}
+      className={`plc-flex plc-items-center plc-justify-center plc-h-12 plc-px-5 plc-text-gray-700 plc-rounded-md pelcro-google-login plc-shadow-md_dark plc-flex-1 plc-bg-white hover:plc-bg-transparent ${className}`}
     >
       <PasswordlessLogoIcon
-        className={`plc-w-6 plc-h-auto" ${iconClassName}`}
+        className={`plc-w-6 plc-h-auto pelcro-paswordless-login-icon" ${iconClassName}`}
       />
-      <p className={`${labelClassName}`}>
+      <p
+        className={`pelcro-paswordless-login-label plc-ml-2 ${labelClassName}`}
+      >
         {name ?? t("title")}
       </p>
     </button>
