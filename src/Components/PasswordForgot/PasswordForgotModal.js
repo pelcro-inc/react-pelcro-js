86729b67a02031e7d5a75bb9cf9736bf7e27f96d
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2022-08-02 11:48:07 +0200

    feat: redesign the forget password modal

--- src/Components/PasswordForgot/PasswordForgotModal.js
+++ src/Components/PasswordForgot/PasswordForgotModal.js
@@ -4,6 +4,7 @@ import { usePelcro } from "../../hooks/usePelcro";
 import { Link } from "../../SubComponents/Link";
 import {
   Modal,
+  ModalHeader,
   ModalBody,
   ModalFooter
 } from "../../SubComponents/Modal";
@@ -24,20 +25,28 @@ export const PasswordForgotModal = ({
       onDisplay={onDisplay}
       onClose={onClose}
     >
+      <ModalHeader>
+        <div className="plc-text-center plc-text-gray-900 pelcro-title-wrapper plc-flex-1 plc-flex plc-flex-col plc-justify-center">
+          <h4 className="plc-text-2xl plc-font-semibold">
+            {t("title")}
+          </h4>
+        </div>
+      </ModalHeader>
       <ModalBody>
         <PasswordForgotView {...otherProps} />
       </ModalBody>
       <ModalFooter>
-        <div>
-          {t("messages.alreadyHaveAccount") + " "}
+        <p className="plc-mb-9">
+          <span className="plc-font-medium">
+            {t("messages.alreadyHaveAccount") + " "}
+          </span>
           <Link
             id="pelcro-link-login"
             onClick={() => switchView("login")}
           >
             {t("messages.loginHere")}
           </Link>
-        </div>
-        <Authorship />
+        </p>
       </ModalFooter>
     </Modal>
   );
--- src/Components/PasswordForgot/PasswordForgotView.js
+++ src/Components/PasswordForgot/PasswordForgotView.js
@@ -9,11 +9,6 @@ export const PasswordForgotView = (props) => {
   const { t } = useTranslation("passwordForgot");
   return (
     <div id="pelcro-password-forgot-view">
-      <div className="plc-mb-6 plc-text-center plc-text-gray-900 pelcro-title-wrapper">
-        <h4 className="plc-text-2xl plc-font-semibold">
-          {t("title")}
-        </h4>
-      </div>
       <form
         action="javascript:void(0);"
         className="plc-mt-2 pelcro-form"
