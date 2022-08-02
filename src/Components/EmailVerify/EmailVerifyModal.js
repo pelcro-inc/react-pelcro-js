2be1db8aa4547fad2203ca486111d1fc91a9becd
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2022-08-02 12:27:20 +0200

    feat: redesign email verification and passwordless modals

--- src/Components/EmailVerify/EmailVerifyModal.js
+++ src/Components/EmailVerify/EmailVerifyModal.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Authorship from "../common/Authorship";
 import { EmailVerifyView } from "./EmailVerifyView";
 import {
   Modal,
+  ModalHeader,
   ModalBody,
   ModalFooter
 } from "../../SubComponents/Modal";
@@ -12,18 +14,25 @@ export const EmailVerifyModal = ({
   onClose,
   ...otherProps
 }) => {
+  const { t } = useTranslation("verifyEmail");
+
   return (
     <Modal
       onDisplay={onDisplay}
       onClose={onClose}
       id="pelcro-email-verify-modal"
     >
+      <ModalHeader>
+        <div className="plc-text-center plc-text-gray-900 pelcro-title-wrapper plc-flex-1 plc-flex plc-flex-col plc-justify-center">
+          <h4 className="plc-text-2xl plc-font-semibold">
+            {t("labels.title")}
+          </h4>
+        </div>
+      </ModalHeader>
       <ModalBody>
         <EmailVerifyView {...otherProps} />
       </ModalBody>
-      <ModalFooter>
-        <Authorship />
-      </ModalFooter>
+      <ModalFooter></ModalFooter>
     </Modal>
   );
 };
