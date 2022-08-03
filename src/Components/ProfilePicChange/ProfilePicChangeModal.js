2b8d9a2f23b2dc159adfbddab6dbaf74f8db8d25
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2022-08-03 13:13:20 +0200

    feat: redesign update user profile and update profile picture modals

--- src/Components/ProfilePicChange/ProfilePicChangeModal.js
+++ src/Components/ProfilePicChange/ProfilePicChangeModal.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { ProfilePicChangeView } from "./ProfilePicChangeView";
-import Authorship from "../common/Authorship";
 import {
   Modal,
+  ModalHeader,
   ModalBody,
   ModalFooter
 } from "../../SubComponents/Modal";
@@ -15,18 +16,26 @@ export function ProfilePicChangeModal({
   onClose,
   ...otherProps
 }) {
+  const { t } = useTranslation("userEdit");
+
   return (
     <Modal
       id="pelcro-profile-picture-modal"
       onDisplay={onDisplay}
       onClose={onClose}
     >
+      <ModalHeader>
+        <div className="plc-text-center plc-text-gray-900 pelcro-title-wrapper plc-flex-1 plc-flex plc-flex-col plc-justify-center">
+          <h4 className="plc-text-2xl plc-font-semibold">
+            {t("labels.cropperTitle")}
+          </h4>
+          <p>{t("labels.cropperSubtitle")}</p>
+        </div>
+      </ModalHeader>
       <ModalBody>
         <ProfilePicChangeView {...otherProps} />
       </ModalBody>
-      <ModalFooter>
-        <Authorship />
-      </ModalFooter>
+      <ModalFooter></ModalFooter>
     </Modal>
   );
 }
--- src/Components/ProfilePicChange/ProfilePicChangeView.js
+++ src/Components/ProfilePicChange/ProfilePicChangeView.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 import { AlertWithContext } from "../../SubComponents/AlertWithContext";
 import { ProfilePicChangeContainer } from "./ProfilePicChangeContainer";
 import { ProfilePicChangeWrapper } from "./ProfilePicChangeWrapper";
 
 export const ProfilePicChangeView = (props) => {
-  const { t } = useTranslation("userEdit");
-
   return (
     <div id="pelcro-profile-picture-view">
-      <div className="plc-mb-6 plc-text-center plc-text-gray-900 pelcro-title-wrapper">
-        <h4 className="plc-text-2xl plc-font-semibold">
-          {t("labels.cropperTitle")}
-        </h4>
-        <p>{t("labels.cropperSubtitle")}</p>
-      </div>
       <form
         action="javascript:void(0);"
         className="plc-mt-2 pelcro-form"
