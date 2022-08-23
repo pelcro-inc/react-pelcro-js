97e39b5d97fde34442816205c3b6d990e36f9cde
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2022-08-23 19:25:33 +0200

    feat: add plans carousel

--- src/SubComponents/Carousel.js
+++ src/SubComponents/Carousel.js
@@ -2,13 +2,18 @@ import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
-export function Carousel({ items, responsive, ...otherProps }) {
+export function Carousel({
+  items,
+  responsive,
+  controlsStrategy,
+  ...otherProps
+}) {
   return (
     <AliceCarousel
       mouseTracking
       items={items}
       responsive={responsive}
-      controlsStrategy="alternate"
+      controlsStrategy={controlsStrategy}
     />
   );
 }
