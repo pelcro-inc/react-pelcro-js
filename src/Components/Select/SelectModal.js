1439a6468433c9920c7803306371c64d52dc53f2
Mohamed Khaled <104787713+pelcro-mkhaled@users.noreply.github.com>
2022-11-03 14:51:29 +0200

    fix: from login if select empty go to register (#215)

--- src/Components/Select/SelectModal.js
+++ src/Components/Select/SelectModal.js
@@ -107,6 +107,13 @@ class SelectModal extends Component {
       });
     }
 
+    if (
+      this.state.productList.length === 0 &&
+      !window.Pelcro.user.isAuthenticated()
+    ) {
+      this.props.setView('register');
+    }
+
     document.addEventListener("keydown", this.handleSubmit);
   };
 
