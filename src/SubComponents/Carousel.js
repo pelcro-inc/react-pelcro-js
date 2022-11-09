37723971d65824ca40b744daaadd2b935408d66b
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2022-11-09 12:33:03 +0200

    feat: edit carousel css

--- src/SubComponents/Carousel.js
+++ src/SubComponents/Carousel.js
@@ -1,7 +1,6 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import "./Carousel.css";
 
 export function Carousel({
   items,
