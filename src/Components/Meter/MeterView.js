4fe011859b922f2ab57c69d8727b76cfbfeb1105
Mohamed Khaled <mohamed.khaled@pelcro.com>
2022-11-21 15:47:49 +0200

    feat: add new meter design

--- src/Components/Meter/MeterView.js
+++ src/Components/Meter/MeterView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { usePelcro } from "../../hooks/usePelcro";
+import { Button } from "../../SubComponents/Button";
 import { Link } from "../../SubComponents/Link";
 
 export const MeterView = () => {
@@ -20,28 +21,20 @@ export const MeterView = () => {
         {title}
       </h4>
       <p className="plc-text-sm plc-text-gray-600">
-        {subtitle}{" "}
-        <Link
-          className="plc-ml-1"
-          onClick={() => switchView("plan-select")}
-        >
-          {t("messages.subscribeNow")}
-        </Link>
-        {!isAuthenticated() && (
-          <>
-            <br />
-            <span>
-              {t("messages.alreadyHaveAccount") + " "}
-              <Link
-                className="plc-ml-1"
-                onClick={() => switchView("login")}
-              >
-                {t("messages.loginHere")}
-              </Link>
-            </span>
-          </>
-        )}
+        {subtitle},
+        {!isAuthenticated() &&
+          " or " + t("messages.alreadyHaveAccount")}
       </p>
+      <div className="plc-flex plc-mt-2">
+        <Button className="plc-w-1/2" onClick={() => switchView("plan-select")}>
+          {t("messages.subscribeNow")}
+        </Button>
+        {!isAuthenticated() && (
+          <Button className="pelcro-button-ghost plc-w-1/2" onClick={() => switchView("login")}>
+            {t("messages.loginHere")}
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
