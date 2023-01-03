5cb355442a692ec76060fcfebb53dd742e3a369e
Shaker Hamdi <106151230+Shaker-Pelcro@users.noreply.github.com>
2023-01-03 18:23:43 +0200

    feat: add code splitting to all modals

--- src/Components/PaymentMethodUpdate/PaymentMethodUpdateModal.js
+++ src/Components/PaymentMethodUpdate/PaymentMethodUpdateModal.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { useTranslation } from "react-i18next";
 import ReactGA from "react-ga";
-import { PaymentMethodUpdateView } from "./PaymentMethodUpdateView";
 import {
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter
 } from "../../SubComponents/Modal";
+// import { PaymentMethodUpdateView } from "./PaymentMethodUpdateView";
+const PaymentMethodUpdateView = lazy(() =>
+  import("./PaymentMethodUpdateView").then((module) => {
+    return { default: module.PaymentMethodUpdateView };
+  })
+);
 
 export const PaymentMethodUpdateModal = (props) => {
   const { t } = useTranslation("paymentMethod");
@@ -37,7 +42,9 @@ export const PaymentMethodUpdateModal = (props) => {
       </ModalHeader>
 
       <ModalBody>
-        <PaymentMethodUpdateView {...props} onSuccess={onSuccess} />
+        <Suspense fallback={<p>Loading ...</p>}>
+          <PaymentMethodUpdateView {...props} onSuccess={onSuccess} />
+        </Suspense>
       </ModalBody>
 
       <ModalFooter></ModalFooter>
