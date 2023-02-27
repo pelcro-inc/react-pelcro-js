43b3943df44af673137659ff4cc391b5a5a2da94
Mohamed Khaled <mohamed.khaled@pelcro.com>
2023-02-27 16:15:49 +0200

    feat: add createNewTapCard login

--- src/Components/PaymentMethod/PaymentMethodContainer.js
+++ src/Components/PaymentMethod/PaymentMethodContainer.js
@@ -158,6 +158,113 @@ const PaymentMethodContainerWithoutStripe = ({
   }, []);
 
   /*====== Start Tap integration ========*/
+  function createNewTapCard(gatewayToken) {
+    // window.Pelcro.payment.verify(
+    //   {
+    //     auth_token: window.Pelcro.user.read().auth_token,
+    //     first_name:
+    //       window.Pelcro.user.read().first_name || state.firstName,
+    //     last_name:
+    //       window.Pelcro.user.read().last_name || state.lastName,
+    //     phone: window.Pelcro.user.read().phone || state.phone,
+    //     site_id: window.Pelcro.siteid,
+    //     currency:
+    //       plan?.currency ||
+    //       invoice?.currency ||
+    //       window.Pelcro.site.read().default_currency,
+    //     tap_token: gatewayToken,
+    //     redirect_url: `${
+    //       window.Pelcro.environment.domain
+    //     }/webhook/tap/callback/3dsecure?auth_token=${
+    //       window.Pelcro.user.read().auth_token
+    //     }`
+    //   },
+    //   (err, res) => {
+    //     if (err) {
+    //       // Inform the user if there was an error
+    //       onFailure(err);
+    //       dispatch({ type: DISABLE_SUBMIT, payload: false });
+    //       dispatch({ type: LOADING, payload: false });
+    //       return dispatch({
+    //         type: SHOW_ALERT,
+    //         payload: {
+    //           type: "error",
+    //           content: getErrorMessages(err)
+    //         }
+    //       });
+    //     } else {
+    //       toggleAuthenticationPendingView(true, res);
+
+    //       const listenFor3DSecureCompletionMessage = () => {
+    //         const retrieveSourceInfoFromIframe = (event) => {
+    //           const { data } = event;
+    //           if (data.message === "3DS-authentication-complete") {
+    //             const tapID = data.tapID;
+    //             toggleAuthenticationPendingView(false);
+    //             toggleAuthenticationSuccessPendingView(true);
+
+    //             window.removeEventListener(
+    //               "message",
+    //               retrieveSourceInfoFromIframe
+    //             );
+
+    //             dispatch({
+    //               type: SHOW_ALERT,
+    //               payload: {
+    //                 type: "error",
+    //                 content: null
+    //               }
+    //             });
+                
+    //             console.log(tapID);
+
+    //             window.Pelcro.source.create(
+    //               {
+    //                 auth_token: window.Pelcro.user.read().auth_token,
+    //                 token: tapID,
+    //                 gateway: "tap"
+    //               },
+    //               (err, res) => {
+    //                 dispatch({ type: DISABLE_SUBMIT, payload: false });
+    //                 dispatch({ type: LOADING, payload: false });
+    //                 if (err) {
+    //                   onFailure(err);
+    //                   return dispatch({
+    //                     type: SHOW_ALERT,
+    //                     payload: {
+    //                       type: "error",
+    //                       content: getErrorMessages(err)
+    //                     }
+    //                   });
+    //                 }
+            
+    //                 dispatch({
+    //                   type: SHOW_ALERT,
+    //                   payload: {
+    //                     type: "success",
+    //                     content: t("messages.sourceUpdated")
+    //                   }
+    //                 });
+    //                 onSuccess(res);
+    //               }
+    //             );
+
+    //           }
+    //         };
+
+    //         // listen to injected iframe for authentication complete message
+    //         window.addEventListener(
+    //           "message",
+    //           retrieveSourceInfoFromIframe
+    //         );
+    //       };
+
+    //       listenFor3DSecureCompletionMessage();
+    //     }
+    //   }
+    // );
+  }
+
   function confirmTapPayment(res) {
     if (res?.data?.threeDSecure_url) {
       toggleAuthenticationPendingView(true, res.data);
@@ -270,7 +377,7 @@ const PaymentMethodContainerWithoutStripe = ({
         dispatch
       );
     } else if (type === "updatePaymentSource") {
-      createNewTapCard();
+      createNewTapCard(gatewayToken);
     }
 
     function handleTapSubscription() {
