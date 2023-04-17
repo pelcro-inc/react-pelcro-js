d76eae4d10cb729e5b9cf08d50e7ae26cfa38c34
Shaker Hamdi <shaker@pelcro.com>
2023-04-17 11:40:17 +0200

    feat: add more enhancements to the select modal

--- src/SubComponents/Carousel.js
+++ src/SubComponents/Carousel.js
@@ -3,9 +3,18 @@ import Slider from "react-slick";
 import { ReactComponent as ArrowThinRight } from "../assets/arrow-thin-right.svg";
 import { ReactComponent as ArrowThinLeft } from "../assets/arrow-thin-left.svg";
 
-const NextArrow = ({ onClick, className, size = "normal" }) => (
+const NextArrow = ({
+  onClick,
+  className,
+  size = "normal",
+  mobileArrowDown = false
+}) => (
   <button
-    className={`sm:plc-flex plc-hidden plc-text-gray-500 plc-w-11 plc-h-11 plc-items-center plc-justify-center plc-rounded-full plc-text-sm plc-absolute plc-top-1/2 plc--right-12 plc-transform plc--translate-y-1/2 hover:plc-text-gray-900 ${className}`}
+    className={`plc-flex plc-text-gray-500 plc-w-11 plc-h-11 plc-items-center plc-justify-center plc-rounded-full plc-text-sm plc-absolute plc-transform hover:plc-text-gray-900 ${
+      mobileArrowDown
+        ? "plc-right-0 plc-bottom-0 sm:plc--right-12 sm:plc-top-1/2 sm:plc--translate-y-1/2"
+        : "plc--right-12 plc-top-1/2 plc--translate-y-1/2"
+    } ${className}`}
     onClick={onClick}
   >
     <ArrowThinRight
@@ -23,9 +32,18 @@ const NextArrow = ({ onClick, className, size = "normal" }) => (
   </button>
 );
 
-const PrevArrow = ({ onClick, className, size = "normal" }) => (
+const PrevArrow = ({
+  onClick,
+  className,
+  size = "normal",
+  mobileArrowDown = false
+}) => (
   <button
-    className={`sm:plc-flex plc-hidden plc-text-gray-500 plc-w-11 plc-h-11 plc-items-center plc-justify-center plc-rounded-full plc-text-sm plc-absolute plc-top-1/2 plc--left-12 plc-transform plc--translate-y-1/2 hover:plc-text-gray-900 ${className}`}
+    className={`plc-flex plc-text-gray-500 plc-w-11 plc-h-11 plc-items-center plc-justify-center plc-rounded-full plc-text-sm plc-absolute plc-transform hover:plc-text-gray-900 ${
+      mobileArrowDown
+        ? "plx-left-0 plc-bottom-0 sm:plc--left-12 sm:plc-top-1/2 sm:plc--translate-y-1/2"
+        : "plc--left-12 plc-top-1/2 plc--translate-y-1/2"
+    } ${className}`}
     onClick={onClick}
   >
     <ArrowThinLeft
@@ -51,6 +69,7 @@ export function Carousel({
   slidesCount,
   arrowsSize = "normal",
   paddingSize = "small",
+  mobileArrowDown = false,
   children,
   ...otherProps
 }) {
@@ -66,8 +85,18 @@ export function Carousel({
     speed: 200,
     slidesToShow: slidesToShow,
     slidesToScroll: slidesToScroll,
-    nextArrow: <NextArrow size={arrowsSize} />,
-    prevArrow: <PrevArrow size={arrowsSize} />,
+    nextArrow: (
+      <NextArrow
+        size={arrowsSize}
+        mobileArrowDown={mobileArrowDown}
+      />
+    ),
+    prevArrow: (
+      <PrevArrow
+        size={arrowsSize}
+        mobileArrowDown={mobileArrowDown}
+      />
+    ),
     initialSlide: initialSlide,
     dots: dots,
     customPaging: (i) => (
@@ -93,11 +122,11 @@ export function Carousel({
     <div
       className={`carousel-wrapper plc-relative  ${
         paddingSize === "small"
-          ? "sm:plc-px-8 plc-px-0"
+          ? "sm:plc-px-8"
           : paddingSize === "medium"
-          ? "sm:plc-px-10 plc-px-0"
-          : "sm:plc-px-14 plc-px-0"
-      }`}
+          ? "sm:plc-px-10"
+          : "sm:plc-px-14"
+      } ${mobileArrowDown ? "plc-px-0" : "plc-px-8"}`}
     >
       <Slider ref={ref} {...settings}>
         {children}
