49e129b92a9a6986924a815d95d3626d40e154db
Marwan Ahmed <marwan.ahmed@pelcro.com>
2023-06-27 12:09:27 +0300

    feat: pelcro authorship layout change

--- src/Components/common/Authorship.js
+++ src/Components/common/Authorship.js
@@ -7,9 +7,9 @@ const Authorship = () => {
       href={`https://www.pelcro.com/?utm_source=${window.location.hostname}&utm_medium=direct&utm_campaign=powered_by_pelcro`}
       rel="noopener noreferrer nofollow"
       target="_blank"
-      className="plc-inline-flex plc-flex-col plc-items-center"
+      className="plc-inline-flex plc-flex-row plc-items-center plc-mt-6"
     >
-      <span className="plc-inline-flex plc-mb-2 pelcro-authorship">
+      <span className="plc-inline-flex plc-mr-2 pelcro-authorship">
         Powered by
       </span>
       <AuthorshipIcon className="pelcro-authorship" />
