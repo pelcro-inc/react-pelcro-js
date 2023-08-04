3e1f43448b66e4ef9c217fc3137fd3c138fa1fd8
Marwan-Pelcro <marwan.ahmed@pelcro.com>
2023-08-04 18:49:17 +0300

    feat: add todo

--- src/Components/ApplePayButton/ApplePayButton.js
+++ src/Components/ApplePayButton/ApplePayButton.js
@@ -30,6 +30,7 @@ export const ApplePayButton = ({ onClick, props, ...otherProps }) => {
     plan?.amount ??
     invoice.amount_remaining;
 
+  // TODO: remove console logs
   useEffect(() => {
     if (window.ApplePaySession) {
       // Indicates whether the device supports Apple Pay and whether the user has an active card in Wallet.
