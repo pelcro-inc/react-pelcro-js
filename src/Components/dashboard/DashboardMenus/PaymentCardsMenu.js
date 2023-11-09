8f61a64663e4d0a36b47aeead2443146059782c1
Marwan Ahmed <marwan.ahmed@pelcro.com>
2023-11-09 12:38:58 +0200

    feat: add missing merge conflict code

--- src/Components/dashboard/DashboardMenus/PaymentCardsMenu.js
+++ src/Components/dashboard/DashboardMenus/PaymentCardsMenu.js
@@ -1,19 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { ReactComponent as EditIcon } from "../../../assets/edit.svg";
+import { ReactComponent as TrashCanIcon } from "../../../assets/trash-can.svg";
 import { Card } from "../Card";
 import { AddNew } from "../AddNew";
 import { Button } from "../../../SubComponents/Button";
 import { getPaymentCardIcon } from "../../../utils/utils";
 import { usePelcro } from "../../../hooks/usePelcro";
+import { notify } from "../../../SubComponents/Notification";
 
+// TODO: clean up the code
 export const PaymentCardsMenu = (props) => {
   const { t } = useTranslation("dashboard");
-  const { switchView } = usePelcro();
-  const source = window.Pelcro.user.read().source;
+  const {
+    switchView,
+    setPaymentMethodToEdit,
+    setPaymentMethodToDelete
+  } = usePelcro();
+  const [sources, setSources] = useState([]);
+  // const source = window.Pelcro.user.read().source;
+
+  useEffect(() => {
+    window.Pelcro.paymentMethods.list(
+      {
+        auth_token: window.Pelcro.user.read().auth_token
+      },
+      (err, res) => {
+        if (err) {
+          return console.error(err);
+        }
+
+        if (res) {
+          setSources(res.data);
+        }
+      }
+    );
+  }, []);
 
   const displaySourceCreate = () => {
+    return switchView("payment-method-create");
+  };
+
+  const displaySourceEdit = (e) => {
+    const source = e.currentTarget.dataset.key;
+
+    setPaymentMethodToEdit(source);
     return switchView("payment-method-update");
+  };
+
+  const deletePaymentMethod = (
+    paymentMethodId,
+    onSuccess,
+    onFailure
+  ) => {
+    // disable the Login button to prevent repeated clicks
+    window.Pelcro.paymentMethods.deletePaymentMethod(
+      {
+        auth_token: window.Pelcro.user.read().auth_token,
+        payment_method_id: paymentMethodId
+      },
+      (err, res) => {
+        if (err) {
+          return onFailure?.(err);
+        }
+        onSuccess?.(res);
+      }
+    );
+  };
+
+  const onDeletePaymentMethodClick = (source) => {
+    const isDeletable = source?.deletable;
+
+    if (isDeletable) {
+      notify.confirm(
+        (onSuccess, onFailure) => {
+          deletePaymentMethod(source.id, onSuccess, onFailure);
+        },
+        {
+          confirmMessage: t(
+            "messages.paymentMethodDeletion.isSureToDelete"
+          ),
+          loadingMessage: t("messages.paymentMethodDeletion.loading"),
+          successMessage: t("messages.paymentMethodDeletion.success"),
+          errorMessage: t("messages.paymentMethodDeletion.error")
+        },
+        {
+          closeButtonLabel: t("labels.subCancellation.goBack")
+        }
+      );
+    } else {
+      setPaymentMethodToDelete(source.id);
+      return switchView("payment-method-delete");
+    }
   };
 
   return (
@@ -22,17 +100,15 @@ export const PaymentCardsMenu = (props) => {
       className="plc-max-w-100% md:plc-max-w-60% plc-m-auto"
       title={t("labels.paymentSource")}
     >
-      {source ? (
-        <PaymentCardsItems
-          displaySourceCreate={displaySourceCreate}
-          source={source}
-        />
-      ) : (
-        <AddNew
-          title={t("labels.addCard")}
-          onClick={displaySourceCreate}
-        />
-      )}
+      <PaymentCardsItems
+        displaySourceEdit={displaySourceEdit}
+        onDeletePaymentMethodClick={onDeletePaymentMethodClick}
+        sources={sources}
+      />
+      <AddNew
+        title={t("labels.addCard")}
+        onClick={displaySourceCreate}
+      />
     </Card>
   );
 };
@@ -40,24 +116,38 @@ export const PaymentCardsMenu = (props) => {
 const PaymentCardsItems = (props) => {
   const { t } = useTranslation("dashboard");
 
-  return (
+  return props.sources.map((source, index) => (
     <div
+      key={"dashboard-source-" + source.id}
       className={`plc-py-4 plc-px-6 plc-mt-5 plc-flex plc-items-center plc-justify-between last:plc-mb-0 plc-rounded plc-text-gray-900 pelcro-address-wrapper plc-bg-white plc-shadow-md_dark`}
     >
       <div className="plc-flex plc-items-center">
         <p className="plc-mr-6">
-          {getPaymentCardIcon(props?.source?.properties?.brand)}
+          {getPaymentCardIcon(source?.properties?.brand)}
         </p>
         <p className="plc-ml-1 plc-text-lg plc-tracking-widest">
-          •••• •••• •••• {props?.source?.properties?.last4}
+          •••• •••• •••• {source?.properties?.last4}
         </p>
       </div>
+      <div className="plc-flex-grow"></div>
       <Button
+        id={"pelcro-button-update-source-" + index}
         variant="icon"
         className="plc-text-gray-500"
+        data-key={source.id}
         icon={<EditIcon />}
-        onClick={props?.displaySourceCreate}
+        onClick={props?.displaySourceEdit}
+      ></Button>
+      <Button
+        id={"pelcro-button-update-source-" + index}
+        variant="icon"
+        className="plc-text-gray-500"
+        data-key={source.id}
+        icon={<TrashCanIcon />}
+        onClick={() => {
+          props?.onDeletePaymentMethodClick(source);
+        }}
       ></Button>
     </div>
-  );
+  ));
 };
