beda67ff8f3d6bd8a1b9b7e6eb61a3fbfaa34aa2
Pawel Kuwik <p.kuwik@selleo.com>
2024-12-16 15:21:43 +0100

    feat: add guard

--- src/Components/PaymentMethod/PaymentMethodContainer.js
+++ src/Components/PaymentMethod/PaymentMethodContainer.js
@@ -2892,7 +2892,7 @@ const PaymentMethodContainer = (props) => {
 
   useEffect(() => {
     if (isStripeLoaded && !selectedPaymentMethodId) {
-      window.Pelcro.user.createSetupIntent(
+      window.Pelcro.user.createSetupIntent?.(
         { auth_token: window.Pelcro.user.read().auth_token },
         (err, res) => {
           if (err) {
