871964a842ef49f8171c04b80174d9315d60cb7f
ADMIN <atahoon@pelcro.com>
2025-02-13 20:58:02 +0200

    Fix currency validation bug

--- src/Components/Cart/CartContainer.js
+++ src/Components/Cart/CartContainer.js
@@ -49,33 +49,32 @@ const CartContainer = ({
     );
     const cartHasMultipleCurrencies = itemsCurrencies.size > 1;
 
-    // if (cartHasMultipleCurrencies) {
-    //   dispatch({ type: DISABLE_SUBMIT, payload: true });
-    //   dispatch({
-    //     type: SHOW_ALERT,
-    //     payload: {
-    //       type: "error",
-    //       content: t("messages.multipleCurrencies")
-    //     }
-    //   });
-    // } else {
-    //   const userCurrency = window.Pelcro.user.read().currency;
-    //   const itemsCurrency = cartItems[0].currency;
+    if (cartHasMultipleCurrencies) {
+      dispatch({ type: DISABLE_SUBMIT, payload: true });
+      dispatch({
+        type: SHOW_ALERT,
+        payload: {
+          type: "error",
+          content: t("messages.multipleCurrencies")
+        }
+      });
+    } else {
+      // const userCurrency = window.Pelcro.user.read().currency;
+      // const itemsCurrency = cartItems[0].currency;
 
-    //   if (userCurrency && userCurrency !== itemsCurrency) {
-    //     dispatch({ type: DISABLE_SUBMIT, payload: true });
-    //     dispatch({
-    //       type: SHOW_ALERT,
-    //       payload: {
-    //         type: "error",
-    //         content: t("messages.currencyMismatch", {
-    //           currency: userCurrency.toUpperCase()
-    //         })
-    //       }
-    //     });
-    //   }
-      
-    // }
+      // if (userCurrency && userCurrency !== itemsCurrency) {
+      //   dispatch({ type: DISABLE_SUBMIT, payload: true });
+      //   dispatch({
+      //     type: SHOW_ALERT,
+      //     payload: {
+      //       type: "error",
+      //       content: t("messages.currencyMismatch", {
+      //         currency: userCurrency.toUpperCase()
+      //       })
+      //     }
+      //   });
+      // }
+    }
   }, [cartItems]);
 
   const submit = (state, dispatch) => {
