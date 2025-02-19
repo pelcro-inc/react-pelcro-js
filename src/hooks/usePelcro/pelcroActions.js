ae8884ea0dfd7d631f4af1c5e1e61c8b7eb1030e
atahoon-pelcro <atahoon@pelcro.com>
2025-02-19 15:31:10 +0200

    feat: fix match user account (#415)

--- src/hooks/usePelcro/pelcroActions.js
+++ src/hooks/usePelcro/pelcroActions.js
@@ -347,17 +347,17 @@ export class PelcroActions {
     const skusMatchingUserCurrency =
       window.Pelcro.ecommerce.products.getSkus();
 
-    const itemMatchesUserCurrency = skusMatchingUserCurrency.some(
-      (sku) => sku.id === quickPurchaseItem.id
-    );
+    // const itemMatchesUserCurrency = skusMatchingUserCurrency.some(
+    //   (sku) => sku.id === quickPurchaseItem.id
+    // );
 
-    if (!itemMatchesUserCurrency) {
-      const userCurrency = window.Pelcro.user.read().currency;
-      console.error(
-        `SKU currency (${quickPurchaseItem.currency}) doesn't match user account's currency (${userCurrency}). users can only purchase SKUs that match their account's currency`
-      );
-      return false;
-    }
+    // if (!itemMatchesUserCurrency) {
+    //   const userCurrency = window.Pelcro.user.read().currency;
+    //   console.error(
+    //     `SKU currency (${quickPurchaseItem.currency}) doesn't match user account's currency (${userCurrency}). users can only purchase SKUs that match their account's currency`
+    //   );
+    //   return false;
+    // }
 
     const quickPurchaseItemWithQuantity = {
       ...quickPurchaseItem,
