9205e3f4b54679c0a8bfe1893189d4355500720a
Mohamedyasserzaki <m.yasser@pelcro.com>
2025-06-20 07:38:45 +0300

    feat: hide gifting for renewal with uiSettings hideGiftForAutoRenew

--- src/Components/Select/SelectModal.js
+++ src/Components/Select/SelectModal.js
@@ -32,6 +32,7 @@ export function SelectModalWithHook(props) {
     plan,
     product,
     isRenewingGift,
+    subscriptionIdToRenew,
     switchView,
     resetView,
     view,
@@ -51,6 +52,7 @@ export function SelectModalWithHook(props) {
     <SelectModalWithTrans
       isGift={isGift}
       disableGifting={isRenewingGift || props.hideGiftButton}
+      subscriptionIdToRenew={subscriptionIdToRenew}
       plan={plan}
       product={product}
       onClose={() => {
@@ -626,7 +628,11 @@ class SelectModal extends Component {
   };
 
   renderPlans = () => {
-    const { disableGifting, hideGiftSelection } = this.props;
+    const {
+      disableGifting,
+      hideGiftSelection,
+      subscriptionIdToRenew
+    } = this.props;
     const hideGiftForAutoRenew =
       window?.Pelcro?.uiSettings?.hideGiftForAutoRenew ?? false;
 
@@ -734,7 +740,10 @@ class SelectModal extends Component {
 
                 {!disableGifting &&
                   (!hideGiftForAutoRenew ||
-                    !(plan?.auto_renew ?? false)) && (
+                    !(plan?.auto_renew ?? false)) &&
+                  !(
+                    subscriptionIdToRenew && hideGiftForAutoRenew
+                  ) && (
                     <button
                       className={`plc-flex plc-items-center plc-justify-center plc-text-center plc-py-2 plc-px-4 plc-w-full plc-border-2 plc-rounded-sm plc-border-primary focus:plc-outline-none plc-text-primary plc-bg-white hover:plc-border-primary-600 hover:plc-text-primary-600 hover:plc-shadow-sm plc-transition-all`}
                       data-key={plan.id}
@@ -929,6 +938,10 @@ SelectModal.propTypes = {
   iaGift: PropTypes.bool,
   hideGiftSelection: PropTypes.bool,
   disableGifting: PropTypes.bool,
+  subscriptionIdToRenew: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
   setView: PropTypes.func,
   onClose: PropTypes.func,
   subscribe: PropTypes.func
