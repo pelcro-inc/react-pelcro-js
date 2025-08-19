26e0e953dad23379d20ca863c03ad57a1109db36
adhammahmoud <adham.m11000@gmail.com>
2025-08-19 18:23:30 +0300

    feat: add Facebook login scope permissions for user data

--- src/Components/common/FacebookLoginButton/FacebookLoginButton.js
+++ src/Components/common/FacebookLoginButton/FacebookLoginButton.js
@@ -51,6 +51,7 @@ export const FacebookLoginButton = ({
       appId={window.Pelcro.site.read().facebook_app_id}
       language={getPageOrDefaultLanguage()}
       fields="first_name,last_name,email,picture"
+      scope="first_name,last_name,email,picture"
       callback={onSuccess}
       onFailure={onFailure}
       render={(renderProps) => (
